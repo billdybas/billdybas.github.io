e846d8e2efb19dc9a399dec85d6f7572067af293
Bill Dybas <wmdybas@gmail.com>
2015-11-27 02:41:26 -0500

    Time for some change
    - Add Video Background

---
+++ index.js
@@ -0,0 +1,64 @@
+(function selectVideo(){
+    var videoId;
+    var ids = [
+        103595267,
+        25049692,
+        85104634,
+        53434339
+    ];
+    var video = document.getElementById('video-bg');
+    videoId = ids[Math.floor(Math.random() * ids.length)];
+    videoURL = "https://player.vimeo.com/video/" + videoId + "?autoplay=1&loop=1";
+    video.src = videoURL;
+})();
+
+(function(global){
+    // Video Height & Width
+    var videoHeight = 1080;
+    var videoWidth = 1920;
+
+    // If the Video Doesn't Take Up the Full Frame
+    // or To Hide Controls
+    var innerVideoWidth = 1500;
+    var videoLetterBoxWidth = videoWidth - innerVideoWidth;
+
+    // Aspect Ratio
+    var videoApspectRatio = videoWidth / videoHeight;
+    var innerVideoApspectRatio = innerVideoWidth / videoHeight;
+
+    var documentEl = document.documentElement;
+
+    var video = document.getElementById('video-bg');
+
+    // Resize the Video
+    function resizeVideo(){
+        var width, height, scale;
+        var windowWidth = documentEl.clientWidth;
+        var windowHeight = documentEl.clientHeight;
+        var windowAspectRatio = windowWidth / windowHeight;
+
+        // Mobile Phone Screen Shaped
+        if (windowAspectRatio < innerVideoApspectRatio) {
+            scale = windowWidth / innerVideoWidth;
+            height = windowHeight;
+            width = height * videoApspectRatio;
+        }
+        // Normal Screen Shaped
+        else {
+            scale = windowWidth / innerVideoWidth;
+            width = windowWidth + (videoLetterBoxWidth * scale);
+            height = width / videoApspectRatio;
+        }
+
+        video.style.width = width + 'px';
+        video.style.height = height + 'px';
+    }
+
+    function onWindowResize() {
+      resizeVideo();
+    }
+
+    global.addEventListener('resize', onWindowResize);
+
+    resizeVideo();
+})(window);
