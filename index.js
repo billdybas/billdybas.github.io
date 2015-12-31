ea385aab7b96aea5adcec7a19396cbf3beb09a53
Bill Dybas <wmdybas@gmail.com>
2015-12-30 23:08:25 -0500

    Let's Start Over
    Baby Steps This Time

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-var video;
-var player;
-var currentVideo;
-var videos = [
-    103595267, // The Asteroids Galaxy Tour - My Club
-    25049692,  // Metronomy - The Bay
-    85104634,  // Vance Joy - Riptide
-    53434339,  // Tame Impala - Feels Like We Only Go Backwards
-    85847275   // The Peach Kings - Be Around
-];
+$(document).ready({
+    // Vimeo Video Ids
+    var videos = [
+        103595267, // The Asteroids Galaxy Tour - My Club
+        25049692,  // Metronomy - The Bay
+        85104634,  // Vance Joy - Riptide
+        53434339,  // Tame Impala - Feels Like We Only Go Backwards
+        85847275   // The Peach Kings - Be Around
+    ];
 
-_selectVideo(videos);
-player.addEvent('ready', ready);
+    var player;
+    var currentVideo;
 
-function _selectVideo(ids){
-    video = document.getElementById('video-bg');
+    function _selectVideo(ids){
+        // Pick a random Vimeo Video Id
+        var videoId;
+        videoId = ids[Math.floor(Math.random() * ids.length)];
+        currentVideo = videoId;
 
-    var videoId;
-    videoId = ids[Math.floor(Math.random() * ids.length)];
-    currentVideo = videoId;
+        // Set the Video
+        var video = document.getElementById('video-bg');
+        videoURL = "https://player.vimeo.com/video/" + videoId + "?autoplay=1&loop=1&api=1";
+        video.src = videoURL;
 
-    videoURL = "https://player.vimeo.com/video/" + videoId + "?autoplay=1&loop=1&api=1";
-    video.src = videoURL;
-    
-    player = $f(video);
-}
-
-function ready(){
+        // Set up Vimeo API - Froogaloop
+        player = $f(video);
+    }
 
     // Chooses another video different than the one currently playing
-    $('#refresh').click(function(){
+    function _refresh(){
         var v = [];
         for(var i = 0; i < videos.length; i++){
             if(videos[i] !== currentVideo){
@@ -41,42 +40,25 @@ function ready(){
         document.getElementById('mute').children[0].className = "glyphicon glyphicon-volume-up";
         // Reset the pause button
         document.getElementById('pause').children[0].className = "glyphicon glyphicon-pause";
-    });
+    }
 
     // Mutes the audio on the video
-    $('#mute').click(function(){
-        player.api('getVolume', function(value, player_id){
-            if(value === 0){
-                //Unmute the Video
-                player.api('setVolume', 1);
-                document.getElementById('mute').children[0].className = "glyphicon glyphicon-volume-up";
-            }
-            else if(value === 1){
-                //Mute the Video
-                player.api('setVolume', 0);
-                document.getElementById('mute').children[0].className = "glyphicon glyphicon-volume-off";
-            }
-            console.log(value);
-        });
-    });
+    function _mute(){
+        console.log('Working - Mute');
+    }
 
     // Pauses the video
-    $('#pause').click(function(){
-        player.api('paused', function(value, player_id){
-            if(value === true){
-                //Play the video
-                player.api('play');
-                document.getElementById('pause').children[0].className = "glyphicon glyphicon-pause";
-            }
-            else if(value === false){
-                //Pause the video
-                player.api('pause');
-                document.getElementById('pause').children[0].className = "glyphicon glyphicon-play";
-            }
-            console.log(value);
-        });
+    function _pause(){
+        console.log('Working - Pause');
+    }
+
+    _selectVideo(ids);
+    player.addEvent('ready', function(){
+        $('#refresh').click(_refresh());
+        $('#mute').click(_mute());
+        $('#pause').click(_pause());
     });
-}
+});
 
 (function(global){
     // Video Height & Width
